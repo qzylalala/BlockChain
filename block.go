e07d418a06ede36d0bdd45aa5f3ef60f09da25b5
qzylalala <304228244@qq.com>
2021-10-14 19:30:15 +0800

    (De)Serialize block for storage

--- block.go
+++ block.go
@@ -7,6 +7,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -38,6 +41,29 @@ func newBlock(data string, prevBlockHash []byte) *Block {
 	return block
 }
 
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
+}
 
 
 
