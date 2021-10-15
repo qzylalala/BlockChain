9b9505b9800ff3f7244bd012b9770f1ab135a2d7
qzylalala <304228244@qq.com>
2021-10-15 09:44:18 +0800

    support CLI and add gitignore file

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module blockChain
 
 go 1.15
+
+require (
+	github.com/boltdb/bolt v1.3.1
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+)
--- main.go
+++ main.go
@@ -6,23 +6,10 @@
 
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := newBlockChain()
+	defer bc.db.Close()
 
-	bc.addBlock("Send 1 BTC to qzylalalla")
-	bc.addBlock("Send 2 more BTC to qzylalala")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Previous block hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := newProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
